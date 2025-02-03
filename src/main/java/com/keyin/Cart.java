4808db0ed5de9277d5f367a96cf88f4123bed310
Victoria Breen <victoriabreen8@hotmail.com>
2025-02-03 19:39:44 -0330

    Minor Changes

--- src/main/java/com/keyin/Cart.java
+++ src/main/java/com/keyin/Cart.java
@@ -12,6 +12,38 @@ public class Cart {
         this.items = new ArrayList<>();
     }
 
+    // Add to the cart
+    // Delete from cart
+    // Display The cart
+    // ? maybe total price of the cart
+
+    public void addGame(Game game){
+        items.add(game);
+        // double check if the space is needed in the string
+        System.out.println(game.getTitle() + " has been successfully added to your cart.");
+    }
+
+    public void deleteGames(){
+        items.clear();
+    }
+
+    public void displayCart(){
+        if (items.isEmpty()) {
+            System.out.println("No items in your cart.");
+            return;
+        }
+
+        System.out.println("Shopping Cart Contains:");
+        for (Game game : items){
+            System.out.println(gamr);
+        }
+
+        // Make get total method
+        System.out.print("Total Amount:", getTotal());
+    }
+
+
+
 
 
 
